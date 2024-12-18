157e74c6002dccfe88697d727226c1c5a2940dfa
Jzani <89129406+Jzani@users.noreply.github.com>
2024-12-18 11:25:56 +0530

    Add files via upload

---
+++ myproject/myapp/views.py
@@ -0,0 +1,31 @@
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignUpForm
+
+def home_view(request):
+    return render(request, 'home.html')  # Optional home page
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
